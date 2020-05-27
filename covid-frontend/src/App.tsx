7003ddb9011258765213e87aad0464c18a0bccd6
Jared Clark <rjaredclark@gmail.com>
2020-05-26 21:00:08 -0400

    dummy backend call, scss added

--- covid-frontend/src/App.tsx
+++ covid-frontend/src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const axios = require('axios').default;
+  axios.get('localhost:5000/')
+    .then(function (response: any) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error: any) {
+      // handle error 
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+      console.log('finally...');
+    });
+
   return (
     <div className="App">
       <header className="App-header">
